8b2bb38701ead1e0cc40ad0fb36b07a8a5f3edcd
ariel101 <ariel.cayo.vargas@gmail.com>
2025-04-03 18:59:11 -0400

    ff

---
+++ sistema_textil/resources/js/Components/Layout.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="flex h-screen">
+      <!-- Sidebar -->
+      <Sidebar />
+      <div class="flex-1 flex flex-col overflow-hidden">
+        <!-- Navbar -->
+        <Navbar />
+        <StatisticGrid />
+        <!-- Main Content -->
+        <main class="flex-1 overflow-y-auto p-6 rounded-md mt-2">
+          <slot />
+          
+        </main>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Sidebar from './Sidebar.vue'
+  import Navbar from './NavBar.vue';
+  import StatisticGrid from './StatisticGrid.vue';
+  
+  export default {
+    components: {
+      Sidebar,
+      Navbar,
+      StatisticGrid
+    },
+  }
+  </script>
+  
+  <style scoped>
+  /* Puedes agregar estilos personalizados aquí */
+  </style>
+  
